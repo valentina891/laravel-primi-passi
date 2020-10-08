c4388e38886a1ea839d175b5dd859cc64d31ac21
valentina891 <valemil.vs@gmail.com>
2020-10-09 00:25:33 +0200

    ..molti tentativi dopo..

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+    <img src="https://www.lamolisana.it/wp-content/uploads/2020/04/marchio-sito-test.png" alt="logo">
+    <nav>
+        <ul>
+            <li><a href="{{ route('home') }}">Home</a></li>
+            <li><a href="{{ route('prodotti') }}">Prodotti</a></li>
+            <li><a href="{{ route('contatti') }}">Contatti</a></li>
+        </ul>
+    </nav>
+</header>
